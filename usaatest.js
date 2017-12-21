db77de92d3c0fc0bc4e69ddca028d4374bc8e45c
nicolette <nicolette.williams8@icloud.com>
2017-12-21 11:15:34 -0600

    push

--- usaatest.js
+++ usaatest.js
@@ -1,9 +1,10 @@
-
-var elmnt = document.getElementById("about");
-
-function scrollToTop() {
-    elmnt.scrollIntoView(true); // Top
-};
+function scrollFunction() {
+   document.getElementById("test").classList.add('dark-nav');
+   document.getElementById("ul-list").classList.add('dark-nav');
+ 
+ }
+ 
+ window.onscroll = scrollFunction;
 
 
 (function() {
